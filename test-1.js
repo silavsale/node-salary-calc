f5ab2549ba6c1223d740726149929cdbe070db14
Silavsale <silavsale@gmail.com>
2019-06-02 21:08:05 -0400

    fist commit

---
+++ test-1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'application',
+    user: 'postgres',
+    password: 'u2u2u2',
+});
+
+const fields = ['schemaname', 'tablename', 'tableowner'].join(', ');
+const sql = `SELECT ${fields} FROM pg_tables WHERE tableowner = $1`;
+pool.query(sql, ['marcus'], (err, res) => {
+    if (err) {
+        throw err;
+    }
+    console.dir({ res });
+    console.table(res.fields);
+    console.table(res.rows);
+    pool.end();
+
+
+});
